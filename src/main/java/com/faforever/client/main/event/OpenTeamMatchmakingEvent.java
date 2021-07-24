35a31e9b3565e32d59dbaa03800629fcfa413ca8
Axle1975 <loweam@gmail.com>
2021-07-25 00:15:11 +1000

    Fix navigate-to-matchmaker on accept invite

--- src/main/java/com/faforever/client/main/event/OpenTeamMatchmakingEvent.java
+++ src/main/java/com/faforever/client/main/event/OpenTeamMatchmakingEvent.java
@@ -2,6 +2,6 @@ package com.faforever.client.main.event;
 
 public class OpenTeamMatchmakingEvent extends NavigateEvent {
   public OpenTeamMatchmakingEvent() {
-    super(NavigationItem.PLAY);
+    super(NavigationItem.MATCHMAKER);
   }
 }
